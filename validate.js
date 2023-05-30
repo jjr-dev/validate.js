95432e2be87445275b462b4a46cc987f90c57e72
Julimar Jr <jjunior@xcapitalbank.com.br>
2023-05-29 21:38:22 -0300

    Initial Commit

---
+++ validate.js
@@ -0,0 +1,211 @@
+class Validate {
+  form(form, options = {}) {
+    form = document.querySelector(form);
+
+    if (form) {
+      form.setAttribute("novalidate", true);
+
+      form.addEventListener("submit", (e) => {
+        e.preventDefault();
+
+        this.validateForm(form, options);
+
+        if ("rules" in options) {
+          for (const name in options.rules) {
+            const itemElement = form.querySelector(`[name="${name}"]`);
+
+            ["change", "input"].forEach((event) => {
+              itemElement.addEventListener(event, () => {
+                this.validateForm(form, options, true);
+              });
+            });
+          }
+        }
+      });
+
+      form.addEventListener("reset", (e) => {
+        // TODO: Remover avisos ao resetar
+      });
+    }
+  }
+
+  getFormData(form) {
+    return Object.fromEntries(new FormData(form).entries());
+  }
+
+  showFormErrors(form, errors = {}, parents = {}, reValidate = false) {
+    let focused = false;
+    let hasError = false;
+
+    for (const name in errors) {
+      const messages = errors[name];
+
+      const itemElement = form.querySelector(`[name="${name}"]`);
+
+      if (!itemElement) continue;
+
+      const defaultParents = parents["default"] ?? 0;
+      const itemParents = parseInt(parents[name] ?? defaultParents);
+
+      let parent = itemElement;
+
+      for (let i = 0; i < itemParents; i++) {
+        parent = parent.parentNode;
+      }
+
+      const oldMessage = parent.parentNode.querySelector(
+        "label.form-error-msg"
+      );
+      if (oldMessage) oldMessage.remove();
+
+      itemElement.removeAttribute("invalid");
+      parent.classList.remove("validate-error");
+
+      if (messages.length === 0) continue;
+
+      itemElement.setAttribute("invalid", "");
+
+      const message = messages[0];
+
+      if (message === "") continue;
+
+      parent.insertAdjacentHTML(
+        "afterend",
+        `<label for='${name}' class='form-error-msg'>${message}</label>`
+      );
+
+      parent.classList.add("validate-error");
+
+      if (!reValidate && !focused) {
+        itemElement.focus();
+        focused = true;
+      }
+
+      if (!hasError) hasError = true;
+    }
+
+    return hasError;
+  }
+
+  validateForm(form, options, reValidate = false) {
+    const getErrorMessage = (rule, name, value = false) => {
+      if (!messages[name] || !messages[name][rule]) return "";
+
+      let message = messages[name][rule];
+
+      if (value) message = message.replace("?", value);
+
+      return message;
+    };
+
+    const addError = (name, rule, value = false) => {
+      errors[name].push(getErrorMessage(rule, name, value));
+    };
+
+    const ruleValidate = (rules, rule, name) => {
+      const ruleValue = rules[rule];
+      const itemValue = data[name] ?? "";
+
+      switch (rule) {
+        case "required":
+          if (this.ruleRequired(ruleValue, itemValue)) addError(name, rule);
+          break;
+        case "email":
+          if (this.ruleEmail(ruleValue, itemValue)) addError(name, rule);
+          break;
+        case "minlength":
+          if (this.ruleMinLength(ruleValue, itemValue))
+            addError(name, rule, ruleValue);
+          break;
+        case "maxlength":
+          if (this.ruleMaxLength(ruleValue, itemValue))
+            addError(name, rule, ruleValue);
+          break;
+        case "length":
+          if (!this.ruleLength(ruleValue, itemValue)) addError(name, rule);
+          break;
+        case "equalTo":
+          if (!this.ruleEqualTo(ruleValue, itemValue, form))
+            addError(name, rule);
+          break;
+        case "name":
+          if (!this.ruleName(ruleValue, itemValue)) addError(name, rule);
+          break;
+      }
+    };
+
+    const data = this.getFormData(form);
+
+    const errors = {};
+    const { messages, parents } = options;
+
+    if ("rules" in options) {
+      for (const name in options.rules) {
+        const rules = options.rules[name];
+
+        if (!errors[name]) errors[name] = [];
+
+        for (const rule in rules) {
+          ruleValidate(rules, rule, name);
+        }
+      }
+    }
+
+    if (
+      !this.showFormErrors(form, errors, parents, reValidate) &&
+      !reValidate &&
+      options.submitHandler
+    )
+      options.submitHandler(data, form);
+  }
+
+  isEmpty(str) {
+    return str.trim().length === 0;
+  }
+
+  isAlpha(str, ignoreSpace = false) {
+    if (ignoreSpace) str = str.replace(/\s/g, "");
+    else if (str.includes(" ")) return false;
+
+    str = str.toUpperCase();
+
+    const regex = /^[A-ZÀ-ÖØ-Ý]+$/;
+    return regex.test(str);
+  }
+
+  isEmail(str) {
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return regex.test(str);
+  }
+
+  ruleName(rule, value) {
+    return rule && this.isAlpha(value, true);
+  }
+
+  ruleRequired(rule, value) {
+    return rule && this.isEmpty(value);
+  }
+
+  ruleEmail(rule, value) {
+    return rule && !this.isEmail(value);
+  }
+
+  ruleMinLength(rule, value) {
+    return value.length < parseInt(rule);
+  }
+
+  ruleMaxLength(rule, value) {
+    return value.length > parseInt(rule);
+  }
+
+  ruleLength(rule, value) {
+    return value.length == parseInt(rule);
+  }
+
+  ruleEqualTo(rule, value, form) {
+    const equal = form.querySelector(`[name="${rule}"]`);
+
+    if (!equal) return false;
+    return value == equal.value;
+  }
+}
